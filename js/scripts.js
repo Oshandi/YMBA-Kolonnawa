7e816611a8c1c8efa3876e6d796c5dceb6709093
Piyumi Dissanayake <piyumi.dissanayake@ifsworld.com>
2020-10-13 22:17:54 +0530

    Completed About page

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,31 @@
+$(function() {
+  // Tabs
+  $(".tabgroup > div").hide();
+  $(".tabgroup > div:first-of-type").show();
+  $(".tabs a").click(function(e) {
+    e.preventDefault();
+    var $this = $(this),
+      tabgroup = "#" + $this.parents(".tabs").data("tabgroup"),
+      others = $this
+        .closest("li")
+        .siblings()
+        .children("a"),
+      target = $this.attr("href");
+    others.removeClass("active");
+    $this.addClass("active");
+    $(tabgroup)
+      .children("div")
+      .hide();
+    $(target).show();
 
+    // Scroll to tab content (for mobile)
+    if ($(window).width() < 992) {
+      $("html, body").animate(
+        {
+          scrollTop: $("#first-tab-group").offset().top
+        },
+        200
+      );
+    }
+  });
+});
